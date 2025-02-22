5b9ccdd54c03c2be91cafb08068069f18e515e80
BugHunter-X <askzenai@gmail.com>
2025-02-22 14:58:06 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,6 @@
+To fix this, ensure you remove the observer in the appropriate place:
+
+```objectivec
+@implementation MyClass
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if ([keyPath isEqualToString:@
